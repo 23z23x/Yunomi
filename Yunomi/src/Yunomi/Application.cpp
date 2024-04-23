bf6bdbd2e37c9e5eb6a1f654fc4d6962efc18f66
23z23x <70716998+23z23x@users.noreply.github.com>
2024-04-23 12:04:30 -0500

    Commit to represent changes to Application for testing.

--- Yunomi/src/Yunomi/Application.cpp
+++ Yunomi/src/Yunomi/Application.cpp
@@ -1,12 +1,33 @@
 #include "pch.h"
 
 #include "Application.h"
+#include "Log.h"
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <iostream>
 
 namespace ynm {
 	
 	Application::Application() 
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+		std::cout << extensionCount << " extensions supported\n";
+
+		glm::mat4 matrix;
+		glm::vec4 vec;
+		auto test = matrix * vec;
+
 	}
 
 	Application::~Application() 
@@ -24,3 +45,4 @@ namespace ynm {
 	}
 
 }
+
