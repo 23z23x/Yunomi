24f6c3fef0b60a9da35bba0f713deecfc174f168
23z23x <70716998+23z23x@users.noreply.github.com>
2024-04-23 11:58:09 -0500

    Added some things to pch.h

--- Yunomi/src/pch.h
+++ Yunomi/src/pch.h
@@ -11,3 +11,12 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+
+//Allows GLFW to include Vulkan for us
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+//Local files that shouldn't change (often) and are widely used
+
+#include "Yunomi/Log.h"
+#include "Yunomi/Core.h"
