e974f88b26838b1ac8c99f79ce4fcc289f02c404
23z23x <70716998+23z23x@users.noreply.github.com>
2024-05-23 17:12:27 -0500

    Created Geometry classes, Mesh and Quad.

---
+++ Yunomi/src/Yunomi/Render/Geometry.cpp
@@ -0,0 +1,91 @@
+#include "pch.h"
+#include "Geometry.h"
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+
+namespace ynm
+{
+	Mesh::Mesh(std::string filename)
+	{
+        tinyobj::attrib_t attrib;
+        std::vector<tinyobj::shape_t> shapes;
+        std::vector<tinyobj::material_t> materials;
+        std::string warn, err;
+
+        //Load geometry
+        if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filename.c_str())) {
+            throw std::runtime_error(warn + err);
+        }
+
+        //Unique vertices in shape
+        std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+
+        //for all the shapes in the object (triangles)
+        for (const auto& shape : shapes) {
+            //For every index in the shape
+            for (const auto& index : shape.mesh.indices) {
+                Vertex vertex{};
+
+                //Set the position of a vertex to be the position in 3D space
+                vertex.pos = {
+                    attrib.vertices[3 * index.vertex_index + 0],
+                    attrib.vertices[3 * index.vertex_index + 1],
+                    attrib.vertices[3 * index.vertex_index + 2]
+                };
+                //Set the texture coordinate
+                vertex.texCoord = {
+                    attrib.texcoords[2 * index.texcoord_index + 0],
+                    1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
+                };
+
+                //If this vertex has never been seen before, add it to unique. Otherwise, just add this repeated vertex to indices
+                if (uniqueVertices.count(vertex) == 0) {
+                    uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+                    vertices.push_back(vertex);
+                }
+
+                indices.push_back(uniqueVertices[vertex]);
+            }
+        }
+	}
+
+    Mesh::~Mesh() {}
+
+	std::vector<Vertex> Mesh::getVertices()
+	{
+		return vertices;
+	}
+
+	std::vector<uint32_t> Mesh::getIndices()
+	{
+		return indices;
+	}
+
+    Quad::Quad(float x1, float x2, float y1, float y2, float depth)
+    {
+        if (x2 <= x1 || y1 <= y2)
+        {
+            YNM_CORE_ERROR("Renderer: A Quad object has been defined with invalid coordinates!");
+        }
+
+        vertices = {
+        {{x1, y1, depth}, {1.0f, 0.0f}},
+        {{x1, y2, depth}, {0.0f, 0.0f}},
+        {{x2, y2, depth}, {0.0f, 1.0f}},
+        {{x2, y1, depth}, {1.0f, 1.0f}},
+        };
+    }
+
+    Quad::~Quad() {}
+
+    std::vector<Vertex> Quad::getVertices()
+    {
+        return vertices;
+    }
+
+    std::vector<uint32_t> Quad::getIndices()
+    {
+        return indices;
+    }
+}
---
+++ Yunomi/src/Yunomi/Render/Geometry.h
@@ -0,0 +1,98 @@
+#pragma once
+
+#include "pch.h"
+
+#define GLM_FORCE_RADIANS
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+//For now, keep this for Vertex object. Change soon.
+#include <vulkan/vulkan_core.h>
+
+//This header defines all types of geometry, including generic mesh loading functions and simple square geometry for sprites.
+
+namespace ynm
+{
+	//Vertex definition used in all geometry
+	struct Vertex
+	{
+		glm::vec3 pos;
+		glm::vec2 texCoord;
+
+		static VkVertexInputBindingDescription getBindingDescription() {
+			VkVertexInputBindingDescription bindingDescription{};
+			bindingDescription.binding = 0;
+			bindingDescription.stride = sizeof(Vertex);
+			bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+			return bindingDescription;
+		}
+
+		static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
+			std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
+
+			attributeDescriptions[0].binding = 0;
+			attributeDescriptions[0].location = 0;
+			attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+			attributeDescriptions[0].offset = offsetof(Vertex, pos);
+
+			attributeDescriptions[1].binding = 0;
+			attributeDescriptions[1].location = 1;
+			attributeDescriptions[1].format = VK_FORMAT_R32G32_SFLOAT;
+			attributeDescriptions[1].offset = offsetof(Vertex, texCoord);
+
+			return attributeDescriptions;
+		}
+
+		bool operator==(const Vertex& other) const {
+			return pos == other.pos && texCoord == other.texCoord;
+		}
+	};
+}
+
+//Hash function 
+namespace std {
+	template<> struct hash<ynm::Vertex> {
+		size_t operator()(ynm::Vertex const& vertex) const {
+			return ((hash<glm::vec3>()(vertex.pos) ^
+				(hash<glm::vec2>()(vertex.texCoord) << 1)));
+		}
+	};
+}
+
+namespace ynm
+{
+	//Class for objects of arbitrary shape
+	class Mesh
+	{
+	public:
+		Mesh(std::string filename);
+
+		~Mesh();
+
+		std::vector<Vertex> getVertices();
+		std::vector<uint32_t> getIndices();
+	private:
+		std::vector<Vertex> vertices;
+		std::vector<uint32_t> indices;
+	};
+
+	//Class for quadrangles. Indicies should always be the same. Vertices may change based on side length
+	class Quad
+	{
+	public:
+		Quad(float x1, float x2, float y1, float y2, float depth);
+
+		~Quad();
+			
+		std::vector<Vertex> getVertices();
+		std::vector<uint32_t> getIndices();
+	private:
+		std::vector<Vertex> vertices;
+		const std::vector<uint32_t> indices = {
+			0, 1, 2, 2, 3, 0,
+		};
+	};
+
+}
