9a169146d7de6efecec2c3f7e1e9128d1c43f1d9
23z23x <70716998+23z23x@users.noreply.github.com>
2024-06-04 08:14:20 -0500

    Removed Vulkan-specific vertex attribute info out of Geometry.h.

--- Yunomi/src/Yunomi/Render/Geometry.h
+++ Yunomi/src/Yunomi/Render/Geometry.h
@@ -7,8 +7,6 @@
 #include <glm/gtx/hash.hpp>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-//For now, keep this for Vertex object. Change soon.
-#include <vulkan/vulkan_core.h>
 
 //This header defines all types of geometry, including generic mesh loading functions and simple square geometry for sprites.
 
@@ -19,31 +17,6 @@ namespace ynm
 	{
 		glm::vec3 pos;
 		glm::vec2 texCoord;
-
-		static VkVertexInputBindingDescription getBindingDescription() {
-			VkVertexInputBindingDescription bindingDescription{};
-			bindingDescription.binding = 0;
-			bindingDescription.stride = sizeof(Vertex);
-			bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-
-			return bindingDescription;
-		}
-
-		static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
-			std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
-
-			attributeDescriptions[0].binding = 0;
-			attributeDescriptions[0].location = 0;
-			attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
-			attributeDescriptions[0].offset = offsetof(Vertex, pos);
-
-			attributeDescriptions[1].binding = 0;
-			attributeDescriptions[1].location = 1;
-			attributeDescriptions[1].format = VK_FORMAT_R32G32_SFLOAT;
-			attributeDescriptions[1].offset = offsetof(Vertex, texCoord);
-
-			return attributeDescriptions;
-		}
 
 		bool operator==(const Vertex& other) const {
 			return pos == other.pos && texCoord == other.texCoord;
