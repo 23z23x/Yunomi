1796027466f645e6c81f52e1b86b8b76bef1cd19
23z23x <70716998+23z23x@users.noreply.github.com>
2024-06-15 08:39:11 -0500

    Changed VkClearValue color to be all zeros. The fourth element was a one, which was causing a validation warning.

--- Yunomi/src/Yunomi/Platform/Vulkan/VulkanInstance.cpp
+++ Yunomi/src/Yunomi/Platform/Vulkan/VulkanInstance.cpp
@@ -1115,7 +1115,7 @@ namespace ynm
         renderPassInfo.renderArea.extent = swapChainExtent;
 
         std::array<VkClearValue, 2> clearValues{};
-        clearValues[0].color = { {0.0f, 0.0f, 0.0f, 1.0f} };
+        clearValues[0].color = { {0.0f, 0.0f, 0.0f, 0.0f} };
         clearValues[1].depthStencil = { 1.0f, 0 };
 
         renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
