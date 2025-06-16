c2cbe75be9810c3c141b46c3199f081d81980930
23z23x <1zach2summers@gmail.com>
2025-06-16 10:06:26 -0500

    changed EVENT_CLASS_TYPE macro to use syntax that works regardless of platform.

--- Yunomi/src/Yunomi/Event/Event.h
+++ Yunomi/src/Yunomi/Event/Event.h
@@ -144,9 +144,11 @@ namespace ynm
 	};
 
 	//Macro to implement these without having to retype them every time
-#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
-								virtual EventType GetEventType() const override { return GetStaticType(); }\
-								virtual const char* GetName() const override { return #type; }
+#define EVENT_CLASS_TYPE(type) \
+    static EventType GetStaticType() { return EventType::type; } \
+    virtual EventType GetEventType() const override { return GetStaticType(); } \
+    virtual const char* GetName() const override { return #type; }
+
 
 #define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlag() const override { return category; }
 
