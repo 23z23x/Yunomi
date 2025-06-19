313d97f1a6ada104147c3f6868b5fbe4cdd91ab5
23z23x <1zach2summers@gmail.com>
2025-06-19 07:43:32 -0500

    Adding multiplatform support by creating logic for Linux.

--- Yunomi/src/Yunomi/Core.h
+++ Yunomi/src/Yunomi/Core.h
@@ -1,21 +1,36 @@
 #pragma once
-//Macros for DLL import/export on Windows
 
-#ifdef YNM_PLATFORM_WINDOWS
-	#ifdef YNM_BUILD_DLL
-		#define YNM_API __declspec(dllexport)
-	#else
-		#define YNM_API __declspec(dllimport)
-	#endif
+// Platform detection
+#if defined(_WIN32) || defined(_WIN64)
+    #define YNM_PLATFORM_WINDOWS
+#elif defined(__linux__)
+    #define YNM_PLATFORM_LINUX
 #else
-	//#error Platform not windows
-	#define YNM_API
+    #error "Unsupported platform"
 #endif
 
-#ifdef YNM_ENABLE_ASSERTS
-	#define YNM_ASSERT(x, ...) { if(!(x)) { YNM_ERROR("ASSERT FAILED: {0}", __VA_ARGS__); __debugbreak(); } }
-	#define YNM_CORE_ASSERT(x, ...) { if(!(x)) { YNM_CORE_ERROR("ASSERT FAILED: {0}", __VA_ARGS__); __debugbreak(); } }
+// DLL import/export macros
+#ifdef YNM_PLATFORM_WINDOWS
+    #ifdef YNM_BUILD_DLL
+        #define YNM_API __declspec(dllexport)
+    #else
+        #define YNM_API __declspec(dllimport)
+    #endif
+#elif defined(YNM_PLATFORM_LINUX)
+    #ifdef YNM_BUILD_DLL
+        #define YNM_API __attribute__((visibility("default")))
+    #else
+        #define YNM_API
+    #endif
 #else
-	#define YNM_ASSERT(x, ...)
-	#define YNM_CORE_ASSERT(x, ...)
+    #define YNM_API
+#endif
+
+// Assertion macros
+#ifdef YNM_ENABLE_ASSERTS
+    #define YNM_ASSERT(x, ...) { if(!(x)) { YNM_ERROR("ASSERT FAILED: {0}", __VA_ARGS__); __builtin_trap(); } }
+    #define YNM_CORE_ASSERT(x, ...) { if(!(x)) { YNM_CORE_ERROR("ASSERT FAILED: {0}", __VA_ARGS__); __builtin_trap(); } }
+#else
+    #define YNM_ASSERT(x, ...)
+    #define YNM_CORE_ASSERT(x, ...)
 #endif
--- Yunomi/src/Yunomi/EntryPoint.h
+++ Yunomi/src/Yunomi/EntryPoint.h
@@ -1,17 +1,33 @@
 #pragma once
 
 #ifdef YNM_PLATFORM_WINDOWS
-	
+
 extern ynm::Application* ynm::CreateApplication();
 
 int main(int argc, char** argv) {
+    ynm::Log::Init();
+    YNM_CORE_INFO("YUNOMI");
 
-	ynm::Log::Init();
-	YNM_CORE_INFO("YUNOMI");
-	
-	auto app = ynm::CreateApplication();
-	app->Run();
-	delete app;
+    auto app = ynm::CreateApplication();
+    app->Run();
+    delete app;
+    return 0;
 }
 
+#elif defined(YNM_PLATFORM_LINUX)
+
+extern ynm::Application* ynm::CreateApplication();
+
+int main(int argc, char** argv) {
+    ynm::Log::Init();
+    YNM_CORE_INFO("YUNOMI");
+
+    auto app = ynm::CreateApplication();
+    app->Run();
+    delete app;
+    return 0;
+}
+
+#else
+    #error "Unsupported platform"
 #endif
